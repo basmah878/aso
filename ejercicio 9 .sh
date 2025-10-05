62444bc699af880f34f0235064ac548465ef4e87
basmah878 <elomaribasmah@gmail.com>
2025-10-05 23:04:37 +0200

    Create ejercicio 9 .sh

---
+++ ejercicio 9 .sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "1) Sumar"
+echo "2) Restar"
+echo "3) Multiplicar"
+echo "4) Dividir"
+read -p "Elige una opción: " opcion
+read -p "Introduce el primer número: " num1
+read -p "Introduce el segundo número: " num2
+
+case 
+  1) echo "Resultado: $((num1 + num2))" ;;
+  2) echo "Resultado: $((num1 - num2))" ;;
+  3) echo "Resultado: $((num1 * num2))" ;;
+  4) echo "Resultado: $((num1 / num2))" ;;
+  *) echo "Opción no válida" ;;
+
