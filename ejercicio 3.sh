79529f804d46edb50abf478aab80a38cb9dc8c0d
basmah878 <elomaribasmah@gmail.com>
2025-10-05 22:44:53 +0200

    Update ejercicio 3.sh

--- ejercicio 3.sh
+++ ejercicio 3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ls /etc > listado.txt
 cat listado.txt
-echo "Número de líneas: $(wc -l < listado.txt)"
-echo "Número de palabras: $(wc -w < listado.txt)"
+echo "Número de líneas: (wc -l < listado.txt)"
+echo "Número de palabras: (wc -w < listado.txt)"
